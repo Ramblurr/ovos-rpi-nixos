4179f0539128d5b60c9a681de804182f2c773090
Casey Link <unnamedrambler@gmail.com>
2023-08-23 18:09:02 +0200

    Workaround: modprobe: FATAL: Module sun4i-drm not found in directory
    see: https://github.com/NixOS/nixpkgs/issues/154163

--- config/generic-aarch64/default.nix
+++ config/generic-aarch64/default.nix
@@ -4,4 +4,12 @@
     <nixpkgs/nixos/modules/installer/sd-card/sd-image-aarch64-new-kernel.nix>
     <nixpkgs/nixos/modules/installer/cd-dvd/channel.nix>
   ];
+  # Workaround: https://github.com/NixOS/nixpkgs/issues/154163
+  # modprobe: FATAL: Module sun4i-drm not found in directory
+  nixpkgs.overlays = [
+    (final: super: {
+      makeModulesClosure = x:
+        super.makeModulesClosure (x // {allowMissing = true;});
+    })
+  ];
 }
